428369214d06b545940fa153dabc1354c58525d1
Alex Melbourne <alex_melbourne@hotmail.co.uk>
2012-03-07 17:04:58 +0000

    finished Covert() and wrote most of Update()

--- DeviceManager.cpp
+++ DeviceManager.cpp
@@ -12,12 +12,29 @@ DeviceManager::~DeviceManager()
 {
 }
 //
+//      Convert a direction and rotation into a 'vec4_t' storing flight dynamics
+vec4_t Convert(vec3_t direction, vec_t drotation)
+{
+        /*
+        **      Makes the assumption that the drone uses +1.0f for clockwise
+        **      rotation around an axis.
+        */
+        
+        return { -direction[0], -direction[1], drotation, direction[2] }
+}
+//
 //      Pull, convert and send data
 int DeviceManager::Update()
 {
-        m_input->Tick(); /* alert the input device that there is data */
+        // Pull new data from the drone (camera frames, etc).
         
-        return -1;
+        m_input->Tick();       // Alert the input device that there is data
+        m_input->Update();     // Force the input device to update { x, y, z }
+        
+        // Generated flight dynamics and send them to the drone.
+        m_drone->Update(Convert(m_input->m_flight, m_input->m_dRotation));
+        
+        return 0;
 }
 
 #endif /* _DEVICE_MANAGER_CPP */
